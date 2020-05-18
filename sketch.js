07aea124b75dea9f4ab6687804f2470922dca23b
Shreyas0896 <shreyas.kotla@gmail.com>
2020-05-18 11:51:05 -0500

    Added images for track,ground,and cars. programeed the game to end after a certain car crosses a specific distance

--- sketch.js
+++ sketch.js
@@ -9,6 +9,17 @@ var database;
 var form, player, game;
 
 var cars, car1, car2, car3, car4;
+var track,ground,car1Image,car2Image,car3Image,car4Image;
+
+function preload() {
+
+car1Image = loadImage("images/car1.png");
+car2Image = loadImage("images/car2.png");
+car3Image = loadImage("images/car3.png");
+car4Image = loadImage("images/car4.png");
+track = loadImage("images/track.jpg");
+ground = loadImage("images/ground.png");
+}
 
 
 function setup(){
@@ -28,4 +39,12 @@ function draw(){
     clear();
     game.play();
   }
+
+if(gameState === 2){
+
+  game.end();
+}
+
+
+
 }
